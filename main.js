270c922064e8be769d3134ffbfe41f4ae77181b4
hhwang <wang@ispan.com.tw>
2022-03-31 14:08:34 +0800

    HtmlWebpackPlugin

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //ES6 匯入
 import { person } from './person.js';
 
-
 const myDiv = document.querySelector('#div1');
 myDiv.innerHTML = `<h2>${person.firstName} ${person.lastName}</h2>`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path"); //node.js 內建模組，用來處理路徑的相關功能
+
 //console.log(__dirname);
 module.exports = {
     entry:'./main.js',
     output:{
-        filename:'bundle.js',
+        filename:'bundle.[chunkhash:8].js',
         //__dirname 取得程式執行位置的實際路徑        
         //C:\Users\iii\Documents\workspace\webpack-demo
         path: path.resolve(__dirname, 'dist') 
         //C:\Users\iii\Documents\workspace\webpack-demo\dist
-    }
+    },
+    plugins:[new HtmlWebpackPlugin({
+        template:'./template.html',
+        //filename:'./site.html',
+        abc:'webpack demo',
+        inject:'body'
+    })]
 }
