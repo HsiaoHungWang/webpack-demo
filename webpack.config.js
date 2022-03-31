177b1bc49be8f68a207cd23f6e3d666ed5fc3166
hhwang <wang@ispan.com.tw>
2022-03-31 11:52:17 +0800

    設定 package.json 檔的 scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
-
+const path = require("path"); //node.js 內建模組，用來處理路徑的相關功能
+//console.log(__dirname);
 module.exports = {
     entry:'./main.js',
     output:{
         filename:'bundle.js',
-        //__dirname 取得程式執行位置的實際路徑
+        //__dirname 取得程式執行位置的實際路徑        
         //C:\Users\iii\Documents\workspace\webpack-demo
         path: path.resolve(__dirname, 'dist') 
         //C:\Users\iii\Documents\workspace\webpack-demo\dist
