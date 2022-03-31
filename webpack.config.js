44f2a7e2fe88f4ebda8237b3d2a836d4f2380b5d
hhwang <wang@ispan.com.tw>
2022-03-31 15:23:00 +0800

    mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path"); //node.js 內建模組，用來處理路徑的相關功能
 
 //console.log(__dirname);
 module.exports = {
-    entry:'./main.js',
-    output:{
-        filename:'bundle.[chunkhash:8].js',
+    entry: './main.js',
+    output: {
+        filename: 'bundle.[chunkhash:8].js',
         //__dirname 取得程式執行位置的實際路徑        
         //C:\Users\iii\Documents\workspace\webpack-demo
-        path: path.resolve(__dirname, 'dist') 
+        path: path.resolve(__dirname, 'dist')
         //C:\Users\iii\Documents\workspace\webpack-demo\dist
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/i,
-                use:["style-loader","css-loader"]
+                test: /\.css$/i,
+                //use: ["style-loader", "css-loader"]  //<style></style>
+                use:[MiniCssExtractPlugin.loader,"css-loader"]//<link rel="stylesheet" href="main.???????.css" />
             }
         ]
 
     },
-    plugins:[new HtmlWebpackPlugin({
-        template:'./template.html',
-        //filename:'./site.html',
-        abc:'webpack demo',
-        inject:'body'
-    })],
-    devServer:{
-        port:3000,
-        open:true
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './template.html',
+            //filename:'./site.html',
+            abc: 'webpack demo',
+            inject: 'body'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'main.[chunkhash:8].css'
+        })
+    ],
+    devServer: {
+        port: 3000,
+        open: true
     }
 }
